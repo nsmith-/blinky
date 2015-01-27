cd66dec7f08fd4bd3db54373b218c1fa93efd879
Nick Smith <ncsmith6@wisc.edu>
2015-01-26 21:20:03 -0600

    break routines into functions

--- main.c
+++ main.c
@@ -7,8 +7,99 @@
 
 extern void pushled(void);
 
-int main(void)
-{
+
+#define WIND_VARIABILITY 4
+#define FLAME_AGILITY 5
+#define WIND_CALMNESS_CONSTANT 2
+#define WIND_BASELINE 30
+unsigned char flame, flameprime, i, wind, x, a, b, c;
+unsigned char random() {
+  x++; //x is incremented every round and is not affected by any other variable
+  a = (a ^ c ^ x); //note the mix of addition and XOR
+  b = (b + a); //And the use of very few instructions
+  c = (c + ((b >> 1) ^ a)); // the AES S-Box Operation ensures an even distributon of entropy
+  return (c);
+}
+
+void simpleFlame() {
+  i = i*19+51;
+  grb_buffer[1] = i;
+  i = i*19+51;
+  grb_buffer[4] = i;
+  _delay_ms(100);
+  pushled();
+}
+
+void rgbFade() {
+  for (i=0; i<=255; i++) {
+    grb_buffer[1] = i;
+    grb_buffer[4] = i;
+    _delay_ms(DELAY);
+    pushled();
+  }
+  for (i=0; i<=255; i++) {
+    grb_buffer[0] = i; grb_buffer[1] = 0xff-i;
+    grb_buffer[3] = i; grb_buffer[4] = 0xff-i;
+    _delay_ms(DELAY);
+    pushled();
+  }
+  for (i=0; i<=255; i++) {
+    grb_buffer[2] = i; grb_buffer[0] = 0xff-i;
+    grb_buffer[5] = i; grb_buffer[3] = 0xff-i;
+    _delay_ms(DELAY);
+    pushled();
+  }
+  for (i=0; i<=255; i++) {
+    grb_buffer[2] = 0xff-i;
+    grb_buffer[5] = 0xff-i;
+    _delay_ms(DELAY);
+    pushled();
+  }
+}
+
+void fancyFlame() {
+  // Thanks to https://github.com/EternityForest/CandleFlickerSimulator/blob/master/flicker2.X/main.c
+  //We simulate a gust of wind by setting the wind var to a random value
+  if (random() < WIND_VARIABILITY) {
+    //Make a gust of wind less likely with two random teata because 255 is not enough resolution
+    if(random() > 220)
+    {
+      wind = random();
+    }
+  }
+  //The wind constantly settles towards its baseline value
+  if (wind>WIND_BASELINE) {
+    wind--;
+  }
+  //The flame constantly gets brighter till the wind knocks it down
+  if (flame<255) {
+    flame++;
+  }
+  //Depending on the wind strength and the calmnes modifer we calcuate the odds
+  //of the wind knocking down the flame by setting it to random values
+  if (random() < (wind>>WIND_CALMNESS_CONSTANT)) {
+    flame = random();
+  }
+  //Real flames ook like they have inertia so we use this constant-aproach-rate filter
+  //To lowpass the flame height
+  if (flame > flameprime) {
+    if (flameprime < (255 - FLAME_AGILITY)) {
+      flameprime += FLAME_AGILITY;
+    }
+  } else {
+    if (flameprime > (FLAME_AGILITY)) {
+      flameprime -= FLAME_AGILITY;
+    }
+  }
+  //How do we prevent jittering when the two are equal?
+  //We don't. It adds to the realism.
+  grb_buffer[1] = flameprime>>2;
+  grb_buffer[4] = flameprime>>2;
+  pushled();
+  _delay_ms(DELAY);
+}
+
+int main(void) {
   // Set up registers
   DDRB |= _BV(PB0)|_BV(PB6); // Enable PB0 (rx led), pin 10
   DDRD |= _BV(PD0)|_BV(PD5);
@@ -20,42 +111,8 @@ int main(void)
   grb_buffer[4] = 0x00;
   grb_buffer[5] = 0x00;
 
-  int i = 0;
-  while(1)
-  {
-    i = i*19+51;
-    grb_buffer[1] = i;
-    i = i*19+51;
-    grb_buffer[4] = i;
-    _delay_ms(100);
-    pushled();
-  }
-
   while(1) {
-    for (i=0; i<=255; i++) {
-      grb_buffer[1] = i;
-      grb_buffer[4] = i;
-      _delay_ms(DELAY);
-      pushled();
-    }
-    for (i=0; i<=255; i++) {
-      grb_buffer[0] = i; grb_buffer[1] = 0xff-i;
-      grb_buffer[3] = i; grb_buffer[4] = 0xff-i;
-      _delay_ms(DELAY);
-      pushled();
-    }
-    for (i=0; i<=255; i++) {
-      grb_buffer[2] = i; grb_buffer[0] = 0xff-i;
-      grb_buffer[5] = i; grb_buffer[3] = 0xff-i;
-      _delay_ms(DELAY);
-      pushled();
-    }
-    for (i=0; i<=255; i++) {
-      grb_buffer[2] = 0xff-i;
-      grb_buffer[5] = 0xff-i;
-      _delay_ms(DELAY);
-      pushled();
-    }
+    fancyFlame();
   }
 
   return 0;
